83a4c36cfb5143caa81c9aa8b8e82f22872e3654
michaelchum <michaelhochum@gmail.com>
2013-11-12 16:08:44 -0500

    Added JSON login database and parsing with SBJson framework

--- VitaLogin/VitaLogin/LoginViewController.m
+++ VitaLogin/VitaLogin/LoginViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LoginViewController.h"
+#import "SBJson.h"
 
 @interface LoginViewController ()
 
@@ -15,8 +16,8 @@
 @implementation LoginViewController
 
 /* OPTIONAL CAN REMOVE */
-@synthesize txtUsername = _txtUsername;
-@synthesize txtPassword = _txtPassword;
+@synthesize txtUsername;
+@synthesize txtPassword;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -39,11 +40,86 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) alertStatus:(NSString *)msg :(NSString *)title
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title
+                                                        message:msg
+                                                       delegate:self
+                                              cancelButtonTitle:@"Ok"
+                                              otherButtonTitles:nil, nil];
+    
+    [alertView show];
+}
+
 - (IBAction)loginClicked:(id)sender {
+    @try {
+        
+        if([[txtUsername text] isEqualToString:@""] || [[txtPassword text] isEqualToString:@""] ) {
+            [self alertStatus:@"Please enter both Username and Password" :@"Login Failed!"];
+        } else {
+            NSString *post =[[NSString alloc] initWithFormat:@"username=%@&password=%@",[txtUsername text],[txtPassword text]];
+            NSLog(@"PostData: %@",post);
+            
+            /* CHANGE THE URL POST JSON SERVER */
+            /* http://michaelchum.com/vitalogin.php Username: vitalogin, Password: password */
+            NSURL *url=[NSURL URLWithString:@"http://michaelchum.com/vitalogin.php"];
+            
+            NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+            
+            NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+            
+            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+            [request setURL:url];
+            [request setHTTPMethod:@"POST"];
+            [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+            [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+            [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+            [request setHTTPBody:postData];
+            
+            //[NSURLRequest setAllowsAnyHTTPSCertificate:YES forHost:[url host]];
+            
+            NSError *error = [[NSError alloc] init];
+            NSHTTPURLResponse *response = nil;
+            NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+            
+            NSLog(@"Response code: %d", [response statusCode]);
+            if ([response statusCode] >=200 && [response statusCode] <300)
+            {
+                NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+                NSLog(@"Response ==> %@", responseData);
+                
+                SBJsonParser *jsonParser = [SBJsonParser new];
+                NSDictionary *jsonData = (NSDictionary *) [jsonParser objectWithString:responseData error:nil];
+                NSLog(@"%@",jsonData);
+                NSInteger success = [(NSNumber *) [jsonData objectForKey:@"success"] integerValue];
+                NSLog(@"%d",success);
+                
+                /* LOGIN SUCCESS is BOOLEAN 1, ADD TRANSITION TO MAIN VIEW UNDER THIS STATEMENT */
+                if(success == 1)
+                {
+                    NSLog(@"Login SUCCESS");
+                    [self alertStatus:@"Logged in Successfully." :@"Login Success!"];
+                    
+                } else {
+                    
+                    NSString *error_msg = (NSString *) [jsonData objectForKey:@"error_message"];
+                    [self alertStatus:error_msg :@"Login Failed!"];
+                }
+                
+            } else {
+                if (error) NSLog(@"Error: %@", error);
+                [self alertStatus:@"Connection Failed" :@"Login Failed!"];
+            }
+        }
+    }
+    @catch (NSException * e) {
+        NSLog(@"Exception: %@", e);
+        [self alertStatus:@"Login Failed." :@"Login Failed!"];
+    }
 }
 
 - (IBAction)backgroundClick:(id)sender {
-    [_txtUsername resignFirstResponder];
-    [_txtPassword resignFirstResponder];
+    [txtUsername resignFirstResponder];
+    [txtPassword resignFirstResponder];
 }
 @end
