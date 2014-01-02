5113a1bc451e3f07cdd87b25212ce785f3b872cc
michaelchum <michaelhochum@gmail.com>
2014-01-01 20:27:11 -0500

    Finalized attributor example

--- Attributor/Attributor/AttributorViewController.m
+++ Attributor/Attributor/AttributorViewController.m
@@ -9,11 +9,55 @@
 #import "AttributorViewController.h"
 
 @interface AttributorViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 @property (weak, nonatomic) IBOutlet UILabel *headline;
 @property (weak, nonatomic) IBOutlet UITextView *body;
 @end
 
 @implementation AttributorViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib (storyboard).
+    
+    // There is no NSMutableAttributedString for UIButton, so we have to make one and set the immutable copy to UIButton
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc]
+                                        initWithString:self.outlineButton.currentTitle];
+    [title addAttributes:@{ NSStrokeWidthAttributeName : @3,
+                            NSStrokeColorAttributeName : self.outlineButton.tintColor }
+                   range:NSMakeRange(0, [title length])];
+    // for C structs, there is usually a NSMake method to make the struct, as for NSRange
+    
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
+
+- (void)preferredFontsChanged:(NSNotification *)notification
+{
+    [self usePreferredFonts];
+}
+
+- (void)usePreferredFonts
+{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
 
 // - (void)addAttribute:value:range:
 - (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
@@ -32,12 +76,6 @@
     [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName : @-3,
                                             NSStrokeColorAttributeName : [UIColor blackColor] }
                                    range:self.body.selectedRange];
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 @end
