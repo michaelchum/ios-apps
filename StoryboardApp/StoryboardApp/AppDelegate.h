63ba0b472d68b74f09a38a592a5dd0f3692ca113
Michael Ho <michaelhochum@gmail.com>
2014-09-08 01:50:30 -0400

    Added practice app with storyboard

---
+++ StoryboardApp/StoryboardApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryboardApp
+//
+//  Created by Michael Ho on 2014-08-20.
+//  Copyright (c) 2014 michaelchum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
